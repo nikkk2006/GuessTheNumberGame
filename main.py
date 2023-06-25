0501c6e33633de29dce1ac441460c6e9511450fe
Nikita Hlinski <hlinski220706@gmail.com>
2023-06-25 13:32:12 +0300

    Добавил функцию endTheWord()

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from random import randint
 
 
+def endTheWord(attempts):
+    end = ""
+
+    if 11 <= attempts % 100 <= 14:
+        end = "ок"
+    elif attempts % 10 == 1:
+        end = "ка"
+    elif 2 <= attempts % 10 <= 4:
+        end = "ки"
+    else:
+        end = "ок"
+
+    return end
+
 def main():
     MIN_VALUE = 1
     MAX_VALUE = 100
@@ -17,9 +31,8 @@ def main():
             print(f"Число {user_number} меньше загаданного числа")
         user_number = int(input("Введите загаданное число: "))
 
-    print(f"Поздравляю, ты угадал число за количество попыток: {attempts}")
-
-
+    ending_of_the_word = endTheWord(attempts)
+    print(f"Поздравляю, ты угадал число и потратил {attempts} попыт{ending_of_the_word}")
 
 
 if __name__ == '__main__':
